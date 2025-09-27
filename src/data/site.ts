deb6aed06aec814b5c9c8a970006c506f7ec86a3
srrma <usrarama@gmail.com>
2025-09-27 19:24:36 +0700

    Cleaning up repetition

---
+++ src/data/site.ts
@@ -0,0 +1,13 @@
+export const SITE_DATA = {
+  bio: "I'm a graphic designer who loves turning photos and anime images into eye-catching posters and designs. My passion lies in creating unique brand identities with logos and visuals that tell a compelling story in a memorable way. I believe that good design is not just about aesthetics, but about clear communication and creating a lasting impact.",
+  shortBio: "I'm a graphic designer who loves turning photos and anime images into eye-catching posters and designs. I also create unique brand identities with logos and visuals that tell your story in a memorable way.",
+  skills: [
+    'Branding & Identity',
+    'Poster Design',
+    'Logo Design',
+    'UI/UX',
+    'Figma',
+    'Adobe Illustrator',
+    'Adobe Photoshop',
+  ],
+};
